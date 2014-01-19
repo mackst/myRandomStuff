f08b21ad3548236b9f3389c7a1a0fdb85dfcb8b2
mackst <schistone@gmail.com>
2014-01-19 19:29:32 +0800

    add how to use

--- cmdReporterHighlighter.py
+++ cmdReporterHighlighter.py
@@ -21,6 +21,12 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# How to use it
+# open script editor and run
+# 
+# import cmdReporterHighlighter as crHighlighter
+# crHighlighter.highlightCmdReporter()
+
 import sys
 import os
 import keyword
